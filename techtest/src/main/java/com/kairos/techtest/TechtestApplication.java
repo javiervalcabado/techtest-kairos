58f6f9633fb9d8128f5c1388409a15fd15840d33
JavierV <javvalcabado@gmail.com>
2024-09-13 19:20:21 +0200

    feat: base commit

---
+++ techtest/src/main/java/com/kairos/techtest/TechtestApplication.java
@@ -0,0 +1,14 @@
+package com.kairos.techtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtestApplication {
+
+	public static void main(String[] args) {
+		System.out.println("aaaaaaaaaaaaaaaaaaaaaaa");
+		SpringApplication.run(TechtestApplication.class, args);
+	}
+
+}
