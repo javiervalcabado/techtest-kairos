8f59b6a6f316356eb6ba5411b8f700916bd85106
JavierV <javvalcabado@gmail.com>
2024-09-16 20:24:35 +0200

    test: added 5 endpoint tests with expected results

---
+++ techtest/src/test/java/com/kairos/techtest/ControllerTests.java
@@ -0,0 +1,68 @@
+package com.kairos.techtest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.kairos.techtest.model.dto.Price;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ControllerTests {
+
+    @LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+    @Test
+	void contextLoads() {
+
+	}
+
+    @SuppressWarnings("null")
+    @Test
+    public void validationTests(){
+
+        // 4 entries in the DB
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/count",
+				String.class)).contains("4");
+
+        ResponseEntity<Price> response = getRestTemplate("2020-06-14 10:00:00", "35455", "1");
+        assertThat(response.getBody().getPriceList()).isEqualTo(1);
+        assertThat(response.getBody().getPrice()).isEqualTo(35.5);
+
+        response = getRestTemplate("2020-06-14 16:00:00", "35455", "1");
+        assertThat(response.getBody().getPriceList()).isEqualTo(2);
+        assertThat(response.getBody().getPrice()).isEqualTo(25.45);
+
+        response = getRestTemplate("2020-06-14 21:00:00", "35455", "1");
+        assertThat(response.getBody().getPriceList()).isEqualTo(1);
+        assertThat(response.getBody().getPrice()).isEqualTo(35.5);
+
+        response = getRestTemplate("2020-06-15 10:00:00", "35455", "1");
+        assertThat(response.getBody().getPriceList()).isEqualTo(3);
+        assertThat(response.getBody().getPrice()).isEqualTo(30.5);
+
+        response = getRestTemplate("2020-06-16 21:00:00", "35455", "1");
+        assertThat(response.getBody().getPriceList()).isEqualTo(4);
+        assertThat(response.getBody().getPrice()).isEqualTo(38.95);
+
+    }
+
+    public ResponseEntity<Price> getRestTemplate(String date, String productID, String brandID) {
+       
+        return this.restTemplate.exchange("/get?date={date}&productID={productID}&brandID={brandID}", HttpMethod.GET, null, Price.class, date, productID, brandID);
+        //this.restTemplate.getForObject("http://localhost:" + port + "/get?date={date}&productID={productID}&brandID={brandID}",
+        //String.class, "2020-06-14 00:00:00", "35455", "1");
+    }
+}
